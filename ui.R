e4db43a741cc21893892185b6749ae00709ab8c6
12miles <tjsharpe@gmail.com>
2015-06-19 14:57:10 -0400

    added more documentation

--- ui.R
+++ ui.R
@@ -5,11 +5,13 @@ shinyUI(fluidPage(
   
   sidebarLayout(
     sidebarPanel(
-      helpText("Create indicator maps with 
+      helpText("This app creates indicator maps with 
                information from the United States Department of Agriculture",
                a("Economic Research Service", href="http://www.ers.usda.gov/data-products/county-level-data-sets/download-data.aspx")),
       
-      
+      helpText("Use the drop-down menu to select which socioeconomic
+               indicator you would like displayed. Your options are 'Percent in Poverty',
+               'Percent Unemployed', and 'Percent with Bacholer'."),
       selectInput("var", 
                   label = "Choose a variable to display",
                   choices = c("Percent in Poverty", "Percent Unemployed",
